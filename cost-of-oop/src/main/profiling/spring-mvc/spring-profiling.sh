ad1c199cc8b22fd07c704825b18972c25a44871b
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-04-16 14:31:30 +0300

    chore: how to build spring project

--- cost-of-oop/src/main/profiling/spring-mvc/spring-profiling.sh
+++ cost-of-oop/src/main/profiling/spring-mvc/spring-profiling.sh
@@ -1,3 +1,10 @@
+## Spring-MVC
+## This script is used to profile the Spring-MVC application using JMeter and YourKit
+## The application is started with the YourKit Java Profiler agent.
+## Before running this script, you need to prepare environment as follows:
+## 1. Build the gs-rest-service project. See README.md for details.
+## 2. Set the path to the application jar in the APPLICATION_JAR variable. Most probably it will be './gs-rest-service/complete/target/rest-service-complete-0.0.1-SNAPSHOT.jar'
+## 3. Then, just run this script.
 export JMETER_PLAN="Spring-MVC.jmx"
-export APPLICATION_JAR="/Users/lombrozo/Workspace/OpenSource/gs-rest-service/complete/target/rest-service-complete-0.0.1-SNAPSHOT.jar"
+export APPLICATION_JAR="./gs-rest-service/complete/target/rest-service-complete-0.0.1-SNAPSHOT.jar"
 ../jar-profiling.sh
