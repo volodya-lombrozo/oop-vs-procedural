004f4b0df68bd9ea7e6252018d2dc6d19272b9f0
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-04-16 14:28:13 +0300

    chore: how to build struts project

--- cost-of-oop/src/main/profiling/struts/apache-struts-example/README.md
+++ cost-of-oop/src/main/profiling/struts/apache-struts-example/README.md
@@ -3,6 +3,16 @@
 It is a simple example of the WEB application based
 on [Struts](https://github.com/apache/struts) framework.
 
+## How to build
+
+To build the application, run the following command:
+
+```shell
+mvn clean package
+```
+
+Then in the `target` directory you will find the `basic-struts.war` file.
+
 ## Ancestor
 
 This submodule was
--- cost-of-oop/src/main/profiling/struts/struts-profiling.sh
+++ cost-of-oop/src/main/profiling/struts/struts-profiling.sh
@@ -1,5 +1,14 @@
+## This script is used to profile the Struts application using JMeter and YourKit
+## The application is started with the YourKit Java Profiler agent.
+## Before running this script, you need to prepare environment as follows:
+## 1. Install Jetty 10.0.15 https://download.eclipse.org/oomph/jetty/release/10.0.15/index.html
+## 2. Set the path to the Jetty home in the JETTY_HOME variable. Usually it is the folder where you installed Jetty: "./jetty-home-10.0.15"
+## 3. Set the path to the Jetty base in the JETTY_BASE variable. Usually it is the 'base' folder in jetty directory: "./jetty-home-10.0.15/base"
+## 4. Build struts-example project. See ./struts-example/README.md for details.
+## 5. Add struts-example.war to the Jetty base webapps folder: cp ./struts-example/target/basic-struts.war ./jetty-home-10.0.15/base/webapps
+## 6. Then, just run this script.
 export JMETER_PLAN="Struts-MVC.jmx"
-export JETTY_HOME="/Users/lombrozo/Workspace/Tools/jetty-home-10.0.15"
-export JETTY_BASE="/Users/lombrozo/Workspace/Tools/jetty-home-10.0.15/base"
+export JETTY_HOME="./jetty-home-10.0.15"
+export JETTY_BASE="./jetty-home-10.0.15/base"
 export APPLICATION_JAR="$JETTY_HOME/start.jar jetty.base=$JETTY_BASE jetty.home=$JETTY_HOME"
 ../jar-profiling.sh
