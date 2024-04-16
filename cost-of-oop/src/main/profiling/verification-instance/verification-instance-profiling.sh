9bc070f927a1de14bb831c3ea719b6a0779e95be
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-04-16 12:46:25 +0300

    chore: how to build verification-instances

--- cost-of-oop/src/main/profiling/verification-instance/verification-instance-profiling.sh
+++ cost-of-oop/src/main/profiling/verification-instance/verification-instance-profiling.sh
@@ -1,2 +1,7 @@
-export APPLICATION_JAR="/Users/lombrozo/Workspace/OpenSource/oop/target/verification-instances.jar"
+## Before specifying application jar path, you need to build the project.
+## If you are in the directory of this script, you can run the following commands:
+## cd ../../java/verification/instances/
+## javac -d ./build *.java
+## jar cfe verification-instances.jar verification.instances.InstanceOnly -C build .
+export APPLICATION_JAR="../../java/verification/instances/verification-instances.jar"
 ../jar-profiling.sh
