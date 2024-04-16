f363020a61787b3e345fde43ed9a88626582d6d6
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-04-16 12:47:55 +0300

    chore: how to build verification-half

--- cost-of-oop/src/main/profiling/verification-half/verification-half-profiling.sh
+++ cost-of-oop/src/main/profiling/verification-half/verification-half-profiling.sh
@@ -1,2 +1,7 @@
-export APPLICATION_JAR="/Users/lombrozo/Workspace/OpenSource/oop/target/verification-half.jar"
+## Before specifying application jar path, you need to build the project.
+## If you are in the directory of this script, you can run the following commands:
+## cd ../../java/verification/half/
+## javac -d ./build *.java
+## jar cfe verification-half.jar verification.half.Half -C build .
+export APPLICATION_JAR="../../java/verification/half/verification-half.jar"
 ../jar-profiling.sh
