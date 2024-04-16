5e9a9cbd8513c77d880686332b9d84ec580f2e49
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-04-16 15:15:28 +0300

    chore: how to build and run dropwizard profiling

--- cost-of-oop/src/main/profiling/dropwizard/dropwizard-profiling.sh
+++ cost-of-oop/src/main/profiling/dropwizard/dropwizard-profiling.sh
@@ -1,3 +1,11 @@
+## Dropwizard
+## This script is used to profile the Dropwizard application using JMeter and YourKit.
+## The application is started with the YourKit Java Profiler agent.
+## Before running this script, you need to prepare environment as follows:
+## 1. Build the dropwizard-example project. See dropwizard-example/README.md for details.
+## 2. Set the path to the application jar in the APPLICATION_JAR variable. Most probably it will be './dropwizard-example/target/dropwizard-1.0-SNAPSHOT.jar'
+## 3. Then, just run this script.
+
 export JMETER_PLAN="Dropwizard.jmx"
-export APPLICATION_JAR="/Users/lombrozo/Workspace/OpenSource/dropwizard-example/target/dropwizard-1.0-SNAPSHOT.jar server"
+export APPLICATION_JAR="./dropwizard-example/target/dropwizard-1.0-SNAPSHOT.jar server"
 ../jar-profiling.sh
