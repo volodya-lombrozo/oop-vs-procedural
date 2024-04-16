bf2fa2c5eec88d77c4a0d47407eb1e4d1225e56f
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-04-16 14:59:03 +0300

    chore: how to build and run micronaut app

--- cost-of-oop/src/main/profiling/micronaut/micronaut-profiling.sh
+++ cost-of-oop/src/main/profiling/micronaut/micronaut-profiling.sh
@@ -1,3 +1,10 @@
+## Micronaut
+## This script is used to profile the Micronaut application using JMeter and YourKit.
+## The application is started with the YourKit Java Profiler agent.
+## Before running this script, you need to prepare environment as follows:
+## 1. Build the micronaut-example project. See micronaut-example/README.md for details.
+## 2. Set the path to the application jar in the APPLICATION_JAR variable. Most probably it will be './micronaut-example/target/demo-0.1.jar'
+
 export JMETER_PLAN="Micronaut.jmx"
-export APPLICATION_JAR="/Users/lombrozo/Workspace/OpenSource/micronaut-example/target/demo-0.1.jar"
+export APPLICATION_JAR="./micronaut-example/target/demo-0.1.jar"
 ../jar-profiling.sh
