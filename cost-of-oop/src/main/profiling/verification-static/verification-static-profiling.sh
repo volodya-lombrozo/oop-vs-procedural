ccfd3199ebcca9e5e2ab12cd7895b6d79f06450f
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-04-16 12:49:46 +0300

    chore: how to build verification-statics

--- cost-of-oop/src/main/profiling/verification-static/verification-static-profiling.sh
+++ cost-of-oop/src/main/profiling/verification-static/verification-static-profiling.sh
@@ -1,2 +1,7 @@
-export APPLICATION_JAR="/Users/lombrozo/Workspace/OpenSource/oop/target/verification-statics.jar"
+## Before specifying application jar path, you need to build the project.
+## If you are in the directory of this script, you can run the following commands:
+## cd ../../java/verification/statics/
+## javac -d ./build *.java
+## jar cfe verification-statics.jar verification.statics.StaticsOnly -C build .
+export APPLICATION_JAR="../../java/verification/statics/verification-statics.jar"
 ../jar-profiling.sh
