7757240faea87b2eaad8e0c90e6ae351a90bf179
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-04-16 12:59:34 +0300

    chore: how to run Apache Tomcat

--- cost-of-oop/src/main/profiling/tomcat/tomcat-profiling.sh
+++ cost-of-oop/src/main/profiling/tomcat/tomcat-profiling.sh
@@ -1,5 +1,8 @@
+## To profile Tomcat, you need to perform the following steps:
+## 1. Install Apache Tomcat 10.1.8 https://tomcat.apache.org/download-10.cgi
+## 2. Set profiling agent to catalina.sh: PROFILER_AGENT="/Applications/YourKit-Java-Profiler-2022.9.app/Contents/Resources/bin/mac/libyjpagent.dylib"
+## (path to the catalina.sh usually is apache-tomcat-10.1.8/bin/catalina.sh)
+## Then don't forget to specify APPLICATION_STARTUP="./apache-tomcat-10.1.8/bin/startup.sh" - to the folder where you installed Apache Tomcat.
 export JMETER_PLAN="Apache-Tomcat.jmx"
-# Agent is set directly in /Users/lombrozo/Workspace/Tools/apache-tomcat-10.1.8/bin/catalina.sh
-#PROFILER_AGENT="/Applications/YourKit-Java-Profiler-2022.9.app/Contents/Resources/bin/mac/libyjpagent.dylib"
-export APPLICATION_STARTUP="/Users/lombrozo/Workspace/Tools/apache-tomcat-10.1.8/bin/startup.sh"
+export APPLICATION_STARTUP="./apache-tomcat-10.1.8/bin/startup.sh"
 ../jar-profiling.sh
