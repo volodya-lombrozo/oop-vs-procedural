f0b6c6a54d8e95a58991b5f5897fb1da11aab90e
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-04-16 12:54:55 +0300

    chore: how to build vert-x-example

--- cost-of-oop/src/main/profiling/vertx/vert-x-example/README.md
+++ cost-of-oop/src/main/profiling/vertx/vert-x-example/README.md
@@ -1,6 +1,15 @@
 # vert-x-example
 It is Vert-X application example for profiling
 
+
+## How to build
+
+To build the application, run the following command:
+
+```shell
+mvn clean package
+```
+
 ## Ancestor
 
 This submodule was
--- cost-of-oop/src/main/profiling/vertx/vertx-profiling.sh
+++ cost-of-oop/src/main/profiling/vertx/vertx-profiling.sh
@@ -1,3 +1,5 @@
+## How to build application jar see
+## ./vert-x-example/README.md
 export JMETER_PLAN="Vert-X.jmx"
-export APPLICATION_JAR="/Users/lombrozo/Workspace/OpenSource/vert-x-example/target/vertx-starter-1.0.0-SNAPSHOT-fat.jar"
+export APPLICATION_JAR="./vert-x-example/target/vertx-starter-1.0.0-SNAPSHOT-fat.jar"
 ../jar-profiling.sh
