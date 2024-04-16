08db181f48501856c60b72f491c44062aea5230e
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-04-16 13:06:45 +0300

    chore: how to build takes-example project

--- cost-of-oop/src/main/profiling/takes/takes-example/README.md
+++ cost-of-oop/src/main/profiling/takes/takes-example/README.md
@@ -3,6 +3,17 @@
 Example of application that uses the [Takes](https://github.com/yegor256/takes)
 framework.
 
+## How to build
+
+To build the application, run the following command:
+
+```shell
+mvn clean package
+```
+
+Then in the `target` directory you will find
+the `takes-example-0.1.0-jar-with-dependencies.jar` file.
+
 ## Ancestor
 
 This submodule was
--- cost-of-oop/src/main/profiling/takes/takes-profiling.sh
+++ cost-of-oop/src/main/profiling/takes/takes-profiling.sh
@@ -1,4 +1,9 @@
+## This script is used to profile the Takes example application.
+## The application is started with the YourKit Java Profiler agent.
+## Specify the path to the profiler agent in the PROFILER_AGENT variable.
+## Specify the path to the application startup command in the APPLICATION_STARTUP variable.
+## ! Before running this script, you need to build the project. See ./takes-example/README.md for details. !
 export JMETER_PLAN="Takes.jmx"
 PROFILER_AGENT="/Applications/YourKit-Java-Profiler-2022.9.app/Contents/Resources/bin/mac/libyjpagent.dylib"
-export APPLICATION_STARTUP="java -agentpath:$PROFILER_AGENT=_instrument_all_methods -Dfile.encoding=UTF-8 -jar /Users/lombrozo/Workspace/OpenSource/takes-example/target/takes-example-0.1.0-jar-with-dependencies.jar"
+export APPLICATION_STARTUP="java -agentpath:$PROFILER_AGENT=_instrument_all_methods -Dfile.encoding=UTF-8 -jar ./takes-example/target/takes-example-0.1.0-jar-with-dependencies.jar"
 ../jar-profiling.sh
