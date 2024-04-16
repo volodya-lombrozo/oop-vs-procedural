34b0b606d4e6b4ca20f693ef6013b26942caa52b
volodya-lombrozo <volodya.lombrozo@gmail.com>
2024-04-16 15:14:22 +0300

    chore: how to run h2 and kafka profiling

--- cost-of-oop/src/main/profiling/h2/h2-profiling.sh
+++ cost-of-oop/src/main/profiling/h2/h2-profiling.sh
@@ -1,3 +1,11 @@
+## H2
+## This script is used to profile the H2 database.
+## The application is started with the YourKit Java Profiler agent.
+## Before running this script, you need to prepare environment as follows:
+## 1. Download H2 2.1.214 from https://h2database.com/html/download.html
+## 2. Set the path to the H2 jar in the APPLICATION_JAR variable in this script. Usually it is the folder where you extracted H2: "./h2/bin/h2-2.1.214.jar"
+## 3. Then, just run this script.
+
 export JMETER_PLAN="H2.jmx"
-export APPLICATION_JAR="/Users/lombrozo/Workspace/Tools/h2/h2-2.1.214.jar"
+export APPLICATION_JAR="./h2/bin/h2-2.1.214.jar"
 ../jar-profiling.sh
--- cost-of-oop/src/main/profiling/kafka/kafka-profiling.sh
+++ cost-of-oop/src/main/profiling/kafka/kafka-profiling.sh
@@ -1,5 +1,12 @@
+## Kafka
+## This script is used to profile Apache Kafka using YourKit Java Profiler.
+## Before running this script, you need to prepare environment as follows:
+## 1. Download Apache Kafka 3.4.0 from https://kafka.apache.org/downloads
+## 2. Set the path to the Kafka home in the KAFKA_PATH variable in this script. Usually it is the folder where you extracted Kafka: "./apache-kafka"
+## 3. Set the path to the YourKit Java Profiler agent in the PROFILER_AGENT variable in this script. Usually it is the folder where you installed YourKit: "/Applications/YourKit-Java-Profiler-2022.9.app/Contents/Resources/bin/mac/libyjpagent.dylib"
+## 4. Then, just run this script.
 export JMETER_PLAN="Apache-Kafka.jmx"
-export KAFKA_PATH=/Users/lombrozo/Workspace/Tools/apache-kafka
+export KAFKA_PATH=./apache-kafka
 $KAFKA_PATH/bin/zookeeper-server-start.sh $KAFKA_PATH/config/zookeeper.properties &
 sleep 10
 export PROFILER_AGENT="/Applications/YourKit-Java-Profiler-2022.9.app/Contents/Resources/bin/mac/libyjpagent.dylib"
